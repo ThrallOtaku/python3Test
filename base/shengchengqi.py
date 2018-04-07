43cabb4aa2286c82e6e7b3b8d12440fe22dbaf04
tanght <tanght@11wlw.com>
2018-04-07 12:28:48 +0800

    python3

--- base/shengchengqi.py
+++ base/shengchengqi.py
@@ -1,1 +1,38 @@
 # coding:utf-8
+# 生成器语法
+
+mylist = [12, 32, 23, 423, 323, 23]
+myiter = iter(mylist)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+for item in mylist:
+    print(item)
+
+
+# 生成器
+def go():
+    print("11")
+    yield 1
+    print("22")
+    yield 2
+    print("33")
+    yield 3
+    print("44")
+    yield 4
+    print("55")
+    yield 5
+    print("66")
+    yield 6
+    print("77")
+    yield 7
+    print("88")
+
+
+T = go()
+print(type(T))
+print("return=", next(T))
+print("return=", next(T))
+print("return=", next(T))
---
+++ python3Character.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+print("hello中国")
+中共=12
+
+print(3/2)
+
+"123".encode() #输入字符串，返回b
+b"".decode() #输入二进制b，返回字符串
